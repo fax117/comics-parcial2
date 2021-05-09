c981211a1c4fe3ff8911c2b520995da1684ca1de
Fabricio <fabricio.fuentes12@gmail.com>
2021-05-09 08:56:48 -0500

    Login & REgister Working

--- app/src/main/java/com/example/comics_parcial2/LoginFragment.kt
+++ app/src/main/java/com/example/comics_parcial2/LoginFragment.kt
@@ -1,10 +1,18 @@
 package com.example.comics_parcial2
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.fragment_login.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,41 +25,65 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+        auth = Firebase.auth
+        //setLoginRegister()
+    }
+
+    // IF LOGGED IN USER GOES DIRECTLY TO MAIN PAGE.
+    override fun onStart() {
+        super.onStart()
+        val activeUser = auth.currentUser
+        if(activeUser != null){
+            loginButton.findNavController().navigate(R.id.action_loginFragment_to_comicListFragment)
+            //activity?.finish()
         }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-                LoginFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        //loginButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_loginFragment_to_mainMenu1Fragment))
+        /*loginButton.setOnClickListener {
+            Log.i("importantInfo", "Hello World!")
+        }*/
+        setLoginRegister()
+    }
+
+    private fun setLoginRegister(){
+        loginButton.setOnClickListener {
+            if(editTextTextEmailAddress.text.isNotEmpty() && editTextTextPassword.text.isNotEmpty()){
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(
+                        editTextTextEmailAddress.text.toString(),
+                        editTextTextPassword.text.toString()
+                ).addOnCompleteListener{
+                    if(it.isSuccessful){
+                        loginButton.findNavController().navigate(R.id.action_loginFragment_to_comicListFragment)
+                        //Navigation.createNavigateOnClickListener(R.id.action_loginFragment_to_mainMenu1Fragment)
+                    }else{
+                        Toast.makeText(context,"User/password incorrect.", Toast.LENGTH_LONG).show()
                     }
                 }
+            }
+            else{
+                //Either email or password is empty
+                Toast.makeText(context,"Please enter an email and password.", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        registerText.setOnClickListener(
+                Navigation.createNavigateOnClickListener(R.id.action_loginFragment_to_registerFragment)
+        )
+
     }
 }
--- app/src/main/java/com/example/comics_parcial2/RegisterFragment.kt
+++ app/src/main/java/com/example/comics_parcial2/RegisterFragment.kt
@@ -5,6 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.navigation.Navigation
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_register.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,16 +22,8 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class RegisterFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -35,25 +32,72 @@ class RegisterFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_register, container, false)
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RegisterFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RegisterFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+//        loginText.setOnClickListener {
+//            fragmentManager?.popBackStackImmediate()
+//        }
+        loginText.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_registerFragment_to_loginFragment)
+        )
+        setupRegister()
+    }
+
+    private fun setupRegister() {
+        registerButton.setOnClickListener {
+            if (registerEmailText.text.isNotEmpty() && registerPwdText.text.isNotEmpty()) {
+                if (registerPwdText.text.toString() == registerPwdText2.text.toString()) {
+                    if (TCCheckbox.isChecked) {
+                        FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+                            registerEmailText.text.toString(),
+                            registerPwdText.text.toString()
+                        ).addOnCompleteListener {
+                            if (it.isSuccessful) {
+                                usuarioCreado()
+                                registerEmailText.text.clear()
+                                registerPwdText.text.clear()
+                                registerPwdText2.text.clear()
+                            }
+                        }.addOnFailureListener {
+                            Toast.makeText(context, it.toString(), Toast.LENGTH_LONG).show()
+                        }
+                    }
+                    else{
+                        Toast.makeText(context, "You need to agree to the Terms and Conditions.", Toast.LENGTH_LONG).show()
+                    }
+                }
+                else{
+                    Toast.makeText(context,
+                        "Passwords don't match.", Toast.LENGTH_LONG).show()
                 }
             }
+            else{
+                Toast.makeText(context,"Mail and password cannot be empty.", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun usuarioCreado(){
+        val builder = AlertDialog.Builder(requireContext())
+        with(builder){
+            setTitle("Registro exitoso")
+            setMessage("¡Nuevo usuario creado con éxito!")
+            /*Revisar que sí te mandé al LOGIN
+            -------------------------------------------------------
+
+
+            /*setPositiveButton("Ok", DialogInterface.OnClickListener { dialog, which ->
+                loginButton.findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
+            })*/
+
+            ---------------------------------------------------------
+             */
+
+            setPositiveButton("Ok",null)
+            show()
+        }
     }
 }
